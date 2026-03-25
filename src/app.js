969e538ccb18964d53c812dd4769d978470fc712
Bhavya Shah <bhavya.khilan@gmail.com>
2026-03-26 00:26:49 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express()
+
+
+// app.use('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.use('/hello', (req, res) => {
+    res.send('Hello from /hello route!')
+})
+
+app.use('/test', (req, res) => {
+    res.send('Hello from /test route!')
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server Started")
+})
