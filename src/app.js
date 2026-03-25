6498fbb7806716ac177641bb4c2b1f5140c48b65
Bhavya Shah <bhavya.khilan@gmail.com>
2026-03-26 00:30:02 +0530

    removed commented code

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express()
 
 
-// app.use('/', (req, res) => {
-//     res.send('Hello World!')
-// })
 
 app.use('/hello', (req, res) => {
     res.send('Hello from /hello route!')
