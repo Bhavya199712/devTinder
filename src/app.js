6859f4c6689814f10ce3284ca7ad54f638fd83cf
Bhavya Shah <bhavya.khilan@gmail.com>
2026-03-26 22:17:52 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -7,10 +7,24 @@ app.use('/hello', (req, res) => {
     res.send('Hello from /hello route!')
 })
 
-app.use('/test', (req, res) => {
+// app.use('/test', (req, res) => {
+//     res.send('Hello from /test route!')
+// })
+
+app.get('/test/:userID', (req, res) => {
+    console.log(req.params.userID)
     res.send('Hello from /test route!')
 })
 
+app.get('/user', (req, res) => {
+  const name = req.query.name || 'guest';
+  const age = req.query.age;
+
+  console.log(req.query);     
+  console.log(name, age);
+
+  res.send(`Hello ${name}! You are ${age || 'unknown age'}.`);
+});
 
 
 app.listen(3000, () => {
